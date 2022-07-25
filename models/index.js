f7efe85c921bbfd856ab24d6d00cfd783008c769
NickSchenck <yoyo4games@yahoo.com>
2022-07-25 14:38:50 -0500

    refactor

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+const Vote = require("./Vote");
 const User = require("./User");
 const Post = require("./Post");
 
@@ -5,9 +6,32 @@ const Post = require("./Post");
 User.hasMany(Post, {
     foreignKey: "user_id"
 });
+//belongsToMany create associations between User and Post models through voting, to see which users voted on a single post or to see which posts a single user voted on
+User.belongsToMany(Post, {
+    through: Vote,
+    as: "voted_posts",
+    foreignKey: "user_id"
+});
+User.hasMany(Vote, {
+    foreignKey: "user_id"
+});
+Post.belongsToMany(User, {
+    through: Vote,
+    as: "voted_posts",
+    foreignKey: "post_id"
+});
 //defines the relationship of Post to User model, this is saying Post can belong to one User not many
 Post.belongsTo(User, {
     foreignKey: "user_id",
 });
+Post.hasMany(Vote, {
+    foreignKey: "post_id"
+});
+Vote.belongsTo(User, {
+    foreignKey: "user_id"
+});
+Vote.belongsTo(Post, {
+    foreignKey: "post_id"
+});
 
-module.exports = { User, Post };
+module.exports = { User, Post, Vote };
