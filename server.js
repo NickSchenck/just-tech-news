d634d2763a30d2dcc02f103438a296c2bea61441
NickSchenck <yoyo4games@yahoo.com>
2022-07-26 18:27:24 -0500

    login/logout

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,7 +10,22 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
 
 const hbs = exphbs.create({});
+const sess = {
+  //hash based msg authentication code
+  secret: "Super secret secret",
+  //instantiates the cookie
+  cookie: {},
+  //resave forces the session to be saved, unless false
+  resave: false,
+  //when new session is made, will save to store
+  saveUnitialized: true,
+  //this will create the connection with the database and allow it to save
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
  
+app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
